d273f6c6ac0ac5c2ef8ff57c0e9e212a7eb295d1
ES Tech Students <estechstudents@utep.edu>
2015-02-17 13:45:11 -0700

    added charts demo

---
+++ js/custom.js
@@ -0,0 +1,44 @@
+﻿
+$(document).ready(function () {
+
+
+
+    $('#demo').on('click', function () {
+        demoChart();
+    })
+
+    function demoChart() {
+        var myGraph = new Chart(document.getElementById("demoChart").getContext("2d")).Line({}, {}),
+            ctx = document.getElementById("demoChart").getContext("2d"),
+            demoData = {
+                labels: ["departmentOne", "departmentTwo", "departmentThree"],
+                datasets: [{
+                    label: "Resolved",
+                    fillColor: "red",
+                    data: [20, 80, 92]
+                },
+                {
+                    label: "Unresolved",
+                    fillColor: "blue",
+                    data: [92, 80, 20]
+                }]
+            }
+        myGraph.destroy();
+        myGraph = new Chart(document.getElementById("demoChart").getContext("2d")).Bar(demoData, {});
+        generateLegend(demoData);
+    }
+    function generateLegend(graph) {
+       
+            var legend = '',
+                datasets = graph.datasets;
+
+            graphClass = "graph-legend";
+            $('#callLogTitle').html("Call Center Data");
+            for (var i = 0; i < graph.datasets.length; i++) {
+                legend += '<li class=graph-legend><span id="' + datasets[i].label.replace(/ /g, "-") + '" class="' + graphClass + '" style="background: '
+                   + graph.datasets[i].fillColor + ';" ></span>   ' + datasets[i].label + '  <i class="graph-legend fa fa-pie-chart fa-spin"></i></li>';
+            }
+
+            $('#legend').html(legend);
+        }
+});
