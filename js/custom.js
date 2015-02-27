d7d92b158803b7df4216837cfbf6df8337279079
ES Tech Students <estechstudents@utep.edu>
2015-02-27 15:42:57 -0700

    Final commit before turning in.

--- js/custom.js
+++ js/custom.js
@@ -1,6 +1,11 @@
 ﻿
 $(document).ready(function () {
 
+    $(function () {
+        highchart();
+        buildTable();
+    })
+
     $('#demo').on('click', function () {
         demoChart();
         demoCircle();
@@ -68,5 +73,91 @@ $(document).ready(function () {
                    + graph.datasets[i].fillColor + ';" ></span>   ' + datasets[i].label + '  <i class="graph-legend fa fa-pie-chart fa-spin"></i></li>';
             }
             $('#legend').html(legend);
+    }
+
+    function highchart() {
+        $('#container').highcharts({
+            chart: {
+                type: 'column'
+            },
+            title: {
+                text: 'Skills/Frameworks'
+            },
+            xAxis: {
+                type: 'category',
+                labels: {
+                    rotation: -45,
+                    style: {
+                        fontSize: '13px',
+                        fontFamily: 'Verdana, sans-serif'
+                    }
+                }
+            },
+            yAxis: {
+                min: 0,
+                title: {
+                    text: 'Skills/Framework experience'
+                }
+            },
+            legend: {
+                enabled: true
+            },
+            tooltip: {
+                pointFormat: 'Experience in months'
+            },
+            series: [{
+                name: 'Experience',
+                data: [
+                    ['Javascript',11 ],
+                    ['CSS', 11],
+                    ['HTML', 11],
+                    ['AngularJS', 4],
+                    ['jQuery', 11],
+                    ['Java', 18],
+                    ['C', 1],
+                    ['C#', 2],
+                    ['SQL', 4],
+                    ['Zurb Foundation Framework', 5],
+                    ['Bootstrap Framework', 4],
+                    ['chartsJS', 5],
+                    ['Visual Studio (Software)',11]
+                ],
+                dataLabels: {
+                    enabled: true,
+                    rotation: -90,
+                    color: '#FFFFFF',
+                    align: 'right',
+                    format: '{point.y:.1f}', // one decimal
+                    y: 10, // 10 pixels down from the top
+                    style: {
+                        fontSize: '13px',
+                        fontFamily: 'Verdana, sans-serif'
+                    }
+                }
+            }]
+        });
+    }
+
+    function buildTable() {
+        var headerText = '<tr>',
+        headers = ['Job Title', 'Responsibilities', 'Time Worked'],
+        jobs = ['Apartment Assistant', 'Dairy Queen Employee', 'ESTG Work-study'],
+        responsibilities =
+        ["Assisted in maintaining apartments. (Cleaning, painting, helping tenants)", "Washed dishes, assisted customers at the register, made orders.",
+        "Set up, and managed the building's conference rooms. Developed web applications for the Enrollment Services Technology Group. Tested/maintained department websites."],
+        timeWorked = ['7/20/2010-8/31/2010', '6/6/2011- 8/20/2011', '1/30/2012-Present'];
+        bodyText = '';
+
+        for (var i = 0; i < headers.length; i++) {
+            headerText += '<th>' + headers[i] + '</th>';
         }
+        headerText += '</tr>';
+
+        for (var j = 0; j < jobs.length; j++) {
+            bodyText += '<tr><td>' + jobs[j] + '</td>' + '<td>' + responsibilities[j] + '</td>' + '<td>' + timeWorked[j] + '</td></tr>';
+        }
+        $('#tableHeaders').html(headerText);
+        $('#tableBody').html(bodyText);
+    }
+
 });
